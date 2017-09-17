ce501e9bb0887bff01413fb92685df7e148092fc
Gabriel Vincent Moon <gabevmoon@gmail.com>
2017-09-17 04:22:35 -0700

    simplified_170917
    Removed a bunch of the scss
    Revised the html (added header.php in space of sidebar.php)
    Updated <nav>s
    Removed a lot of the files that won't be used in the new site
    Added new fonts

--- header.php
+++ header.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	    <meta name="description" content="The motion graphic design portfolio of Gabriel Vincent Moon of Phoenix, Arizona.">
+	    <meta name="keywords" content="Gabe Moon, Gabriel Moon, Arizona, Animation, Design, Graphic Design, Motion Design, Motion Graphic Design, Phoenix, Portfolio, Typography, Video">
+	    <meta name="author" content="Gabriel Vincent Moon">
+	    <link rel="icon" type="image/ico" href="img/gv.ico"/>   
+		<link href="css/style.css" rel="stylesheet" type="text/css" media="screen" />
+		<title>Motion Design by Gabriel Vincent Moon</title>
+	</head>
+	<body>
+		<nav id="header">
+			<a href="http://gabe.mn">GVM</a>
+			<a href="http://gabe.mn"><h4>Gabriel Vincent Moon</h4></a>
+			<a href="http://twitter.com/gvmoon"><img src="img/twitter_icon.svg"></a>
+			<a href="http://instagram.com/leperprint"><img src="img/instagram_icon.svg"></a>
+		</nav>
 
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <link rel="icon" type="image/ico" href="../img/gv.ico"/>
-	<link href="../css/style.css" rel="stylesheet" type="text/css" media="screen" />
-    <link href="../css/video-js.css" rel="stylesheet" type="text/css" media="screen" />
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<title>Design by Gabriel Vincent</title>
-</head>
-
-<body>
--- index.php
+++ index.php
@@ -1,39 +1,59 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta name="description" content="The design portfolio of Gabriel Vincent Moon from Phoenix, Arizona.">
-    <meta name="keywords" content="Gabriel Moon, Gabe Moon, Arizona, Design, Portfolio, Motion, Phoenix, Typography">
-    <meta name="author" content="Gabriel Moon">
-    <link rel="icon" type="image/ico" href="img/gv.ico"/>   
-	<link href="css/style.css" rel="stylesheet" type="text/css" media="screen" />
-	<title>Design by Gabriel Vincent</title>
-</head>
-<script src="js/jquery-1.11.3.js"></script>
-<body>
-    <div class="wrapper">
-		<?php include('sidebar.php'); ?>
-        <div class="main-container">
-			<video onContextMenu="return false;" id="reel" width="100%" height="auto" autoplay controls loop muted>
-			<source src="vid/reel.mp4" type="video/mp4">
-			<source src="vid/reel.webm" type="video/webm">
-			Your browser does not support the video tag.
-			</video>
-            <section class="main" id="main">
-                <a href="urban"><div class="p5" id="p5"></div></a>
-                <a href="klik"><div class="p4" id="p4"></div></a>
-                <a href="ptn"><div class="p9" id="p9"></div></a>
-                <a href="cs"><div class="p6" id="p6"></div></a>
-                <a href="3d"><div class="p8" id="p8"></div></a>
-                <a href="22"><div class="p1" id="p1"></div></a>
-                <a href="object"><div class="p2" id="p2"></div></a>
-                <a href="illus"><div class="p10" id="p10"></div></a>
-                <a href="book"><div class="p3" id="p3"></div></a>
-                <a href="misc"><div class="p7" id="p7"></div></a>
-            </section>
+<?php include('header.php'); ?>
+        <div id="wrapper">
+            <div id="main">
+    			<video onContextMenu="return false;" id="reel" width="100%" height="auto" controls loop muted>
+        			<source src="vid/temp.mp4" type="video/mp4">
+        			<source src="vid/temp.webm" type="video/webm">
+        			Your browser does not support the video tag.
+    			</video>
+                <section id="projects">
+                    <a href="phx">
+                        <div id="p1">
+                            <h1>- City of Phoenix -</h1>
+                            <h2>animation, design, explainer, motion</h2>
+                        </div>
+                    </a>
+                    <a href="som">
+                        <div id="p2">
+                            <h1>- School of Motion -</h1>
+                            <h2>3d, animation, design, motion</h2>
+                        </div>
+                    </a>
+                    <a href="btc">
+                        <div id="p3">
+                            <h1>- Bitcoin -</h1>
+                            <h2>animation, design, explainer, motion</h2>
+                        </div>
+                    </a>
+                    <a href="klk">
+                        <div id="p4">
+                            <h1>- Klik -</h1>
+                            <h2>animation, design, motion</h2>
+                        </div>
+                    </a>
+                    <a href="ptn">
+                        <div id="p5">
+                            <h1>- Protect the Net -</h1>
+                            <h2>animation, design, explainer, motion</h2>
+                        </div>
+                    </a>
+                    <a href="exp">
+                        <div id="p6">
+                            <h1>- Experimental -</h1>
+                            <h2>animation, design, motion</h2>
+                        </div>
+                    </a>
+                </section>
+                <section id="info">
+                    <div class="content">
+                        <img src="img/gabemoon.jpg">
+                        <h3>Gabe Moon</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum.
+        Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet do lore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit. Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit. Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit.</p>
+                    <div class="content">
+                        <img src="img/bridge.jpg">
+                    </div>
+                </section>
+            </div>
         </div>
-    </div>
-
 <?php include('footer.php'); ?>
