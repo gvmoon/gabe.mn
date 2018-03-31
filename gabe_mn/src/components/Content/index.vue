536bac668c89de420bd8db52dcb3debc20d9cca0
Gregory Moon <grmoon@asu.edu>
2018-03-31 15:58:03 -0400

    home done

--- gabe_mn/src/components/Content/index.vue
+++ gabe_mn/src/components/Content/index.vue
@@ -2,46 +2,26 @@
     <div class='gmn-content'>
         <div class='gmn-content-top'>
             <iframe
-                src='https://player.vimeo.com/video/241723499?autoplay=0'
+                :src='video'
                 frameborder='0'
                 webkitAllowFullScreen mozallowfullscreen allowFullScreen
             ></iframe>
         </div>
-        <slot name="middle" />
-        <slot name="bottom" />
+        <slot />
     </div>
 </template>
 
 <style lang='scss' scoped>
-
-.gmn-content {
-    // .gmn-content-top styles from http://embedresponsively.com/
-    & > .gmn-content-top {
-        height: 0;
-        margin-bottom: 1em;
-        max-width: 100%;
-        overflow: hidden;
-        padding-bottom: 56.25%;
-        position: relative;
-
-        & iframe,
-        & object,
-        & embed {
-            height: 100%;
-            left: 0;
-            position: absolute;
-            top: 0;
-            width: 100%;
-        }
-    }
-}
+@import './scss/index.scss';
 </style>
 
 <script>
 export default {
-  props: ['img', 'title'],
-  mounted() {
-
+  props: {
+      video: {
+          required: true,
+          type: String
+      }
   }
 };
 </script>
--- gabe_mn/src/components/Home/index.vue
+++ gabe_mn/src/components/Home/index.vue
@@ -1,76 +1,53 @@
 <template>
-    <AppContent img='static/img/jpg/bridge.jpg' title='Gabe Moon'>
-        <template slot='middle'>
-            <div class='container-fluid'>
-                <div class='row'>
-                    <div class='col-xs-12 col-md-3'>
-                        <div class='row'>
-                            <div class='col-xs-12'>
-                                <div class='gmn-home-header'>
-                                    <div class='gmn-home-header-left'>
-                                        GVM
-                                    </div>
-                                    <div class='gmn-home-header-right'>
-                                        Gabriel Vincent Moon
-                                        <div class='gmn-home-header-right-subtext'>
-                                            Motion Design Generalist
-                                        </div>
+    <AppContent
+        video='https://player.vimeo.com/video/241723499?autoplay=0'
+    >
+        <div class='container-fluid'>
+            <div class='row'>
+                <div class='col-xs-12 col-md-4'>
+                    <div class='row'>
+                        <div class='col-xs-12'>
+                            <div class='gmn-home-header'>
+                                <div class='gmn-home-header-left'>
+                                    GVM
+                                </div>
+                                <div class='gmn-home-header-right'>
+                                    Gabriel Vincent Moon
+                                    <div class='gmn-home-header-right-subtext'>
+                                        Motion Design Generalist
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class='row'>
-                            <div class='col-xs-12'>
-                                3D, Animation, Design, Live, Motion, UX/UI, VFX, Web
-                                Animate, After Eects, Cinema 4D, Illustrator, Photoshop, XD
-                                Motion design is to the 21st century what graphic design was to
-                                the 20th century. e knowledge I gained by studying graphic
-                                design was invaluable to my development as a creative and starting
-                                a freelance motion design business was a no-brainer. As a proud
-                                computer nerd, I love utilizing the latest technology to solve
-                                complex problems for my clients and convey their ideas to the
-                                world. Contact me with freelance opportunities: gabe@gabe.mn
-                            </div>
+                    </div>
+                    <div class='row'>
+                        <div class='col-xs-12 gmn-home-skills'>
+                            3D, Animation, Design, Live, Motion, UX/UI, VFX, Web
+                            Animate, After Eects, Cinema 4D, Illustrator, Photoshop, XD
                         </div>
                     </div>
-                    <div class='col-xs-12 col-md-9'>
-                        <MainLinks />
+                    <div class='row gmn-home-content'>
+                        <div class='col-xs-12'>
+                            Motion design is to the 21st century what graphic design was to
+                            the 20th century. The knowledge I gained by studying graphic
+                            design was invaluable to my development as a creative and starting
+                            a freelance motion design business was a no-brainer. As a proud
+                            computer nerd, I love utilizing the latest technology to solve
+                            complex problems for my clients and convey their ideas to the
+                            world. Contact me with freelance opportunities: <a href='mailto:gabe@gabe.mn'>gabe@gabe.mn</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </template>
-        <template slot='bottom'>
-            <div class='container-fluid'>
-                <div class='row'>
-                    Test
+                <div class='col-xs-12 col-md-8'>
+                    <MainLinks />
                 </div>
             </div>
-        </template>
+        </div>
 </AppContent>
 </template>
 
 <style lang='scss'>
-.gmn-home-header {
-    display: flex;
-
-    & > .gmn-home-header-left {
-        align-items: center;
-        display: flex;
-        font-family: Helvetica;
-        font-size: 1.5em;
-        font-weight: bolder;
-        justify-content: center;
-    }
-
-    & > .gmn-home-header-right {
-        // Same as bootstrap padding-left on column
-        margin-left: 15px;
-
-        & > .gmn-home-header-right-subtext {
-            color: #FFAE71;
-        }
-    }
-}
+@import './scss/index.scss';
 </style>
 
 <script>
