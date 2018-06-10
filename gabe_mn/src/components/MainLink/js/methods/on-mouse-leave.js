2b8db571e555a0bc4d7ef5d2fd8c65cb0531c65b
Gregory Moon <gregory.r.moon@gmail.com>
2018-06-10 16:22:19 -0400

    link mouseenter/mouseleave transitions

--- gabe_mn/src/components/MainLink/js/methods/on-mouse-leave.js
+++ gabe_mn/src/components/MainLink/js/methods/on-mouse-leave.js
@@ -1,4 +1,22 @@
 export default function(mouseLeaveEvent) {
-    this.$refs.gif.style.visibility = 'hidden';
-    this.$refs.content.style.visibility = 'hidden';
+    const gif = this.$refs.gif;
+    const content = this.$refs.content;
+    let opacity = parseFloat(gif.style.opacity);
+
+    gif.style.opacity = opacity;
+    content.style.opacity = opacity;
+
+    const opacityInterval = setInterval(() => {
+      if (opacity == 0) {
+        gif.style.visibility = 'hidden';
+        content.style.visibility = 'hidden';
+        clearInterval(opacityInterval);
+      }
+      else{
+        opacity -= 0.25;
+
+        gif.style.opacity = opacity
+        content.style.opacity = opacity
+      }
+    }, 1000 / 24);
 };
